a075ffcbd7e0d1db52c13c8a51d64ee40ed39b6f
Abby Rauf <abbyrauf.dev@gmail.com>
2025-09-26 05:16:36 -0500

    fix more bugs inside tickets

--- components/checkout/ThankYou.tsx
+++ components/checkout/ThankYou.tsx
@@ -47,6 +47,7 @@ export default function ThankYou() {
                 Your ticket for the <span className="font-semibold text-orange-500">{selectedShow?.title}</span> has been booked successfully.
                 Get ready for the show full of laughter! 😂
             </p>
+            <p>Your tickets have been sent to your email.</p>
             {/* <Button className="bg-gradient-to-r from-orange-500 to-orange-600 hover:from-orange-600 hover:to-orange-700 text-white font-semibold py-3 px-8 rounded-full text-lg shadow-lg transition-all duration-200 transform hover:scale-105 outline-none" onClick={onDownloadTickets}>
                 Download Tickets
             </Button> */}
